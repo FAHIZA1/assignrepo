6877208b5bccc15e9bfa6b1062f83b8d9da36ef5
FAHIZA1 <fahizafreen@gmail.com>
2018-10-17 10:04:18 +0530

    change made to file

--- src/main/java/com/eg/sba/CustomerDAO.java
+++ src/main/java/com/eg/sba/CustomerDAO.java
@@ -1,9 +1,10 @@
 package com.eg.sba;
 
 import java.util.List;
-
+/make change
 interface CustomerDAO
 {
 	public List<Customer> getCustomer();
-	public Customer createCustomer(Customer cust);
+	
 }
+//public Customer createCustomer(Customer cust);
